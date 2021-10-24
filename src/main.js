98906da8b9311e94c2aad16a759c6ac54103546a
Chris Moebus <chris.moebus@moebus-group.de>
2021-10-24 20:08:02 +0200

    added initial graphql query and fetching method

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// AWS Stuff
+import Amplify from 'aws-amplify'
+import aws_exports from './aws-exports.js'
+
+// Configure the AWS Amplify backend
+Amplify.configure(aws_exports)
 
 createApp(App).mount('#app')
